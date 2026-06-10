239581a12dba4f4b5e5a1981ac07db10f77bddc4
Adrian Rodriguez Amago <adrian@binpar.com>
2026-06-10 12:48:33 +0200

    fix public pages without database dependency

--- src/server/api/trpc.ts
+++ src/server/api/trpc.ts
@@ -27,11 +27,9 @@ import { db } from "~/server/db";
  * @see https://trpc.io/docs/server/context
  */
 export const createTRPCContext = async (opts: { headers: Headers }) => {
-  const session = await auth();
-
   return {
     db,
-    session,
+    getSession: auth,
     ...opts,
   };
 };
@@ -120,14 +118,17 @@ export const publicProcedure = t.procedure.use(timingMiddleware);
  */
 export const protectedProcedure = t.procedure
   .use(timingMiddleware)
-  .use(({ ctx, next }) => {
-    if (!ctx.session?.user) {
+  .use(async ({ ctx, next }) => {
+    const session = await ctx.getSession();
+
+    if (!session?.user) {
       throw new TRPCError({ code: "UNAUTHORIZED" });
     }
+
     return next({
       ctx: {
         // infers the `session` as non-nullable
-        session: { ...ctx.session, user: ctx.session.user },
+        session: { ...session, user: session.user },
       },
     });
   });
